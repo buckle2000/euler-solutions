649f2a363c84b77191caf74ff26f5a6874608fb6
buckle2000 <buckle2000@zoho.com>
2018-02-23 20:19:57 +0800

    Solve 002

--- src/001.rs
+++ src/001.rs
@@ -1,6 +1,6 @@
 /// ans = 233168
 fn main() {
-    let ans;
-    ans = (1..1000).filter(|x|x%3==0 || x%5==0).fold(0, |acc,x| acc+x);
+    let ans = (1..1000).filter(|x|x%3==0 || x%5==0)
+        .fold(0, |acc,x| acc+x);
     println!("{}", ans);
 }
